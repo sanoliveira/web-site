76e02e3fdc89c4f626c615fe2ae2af7a170bcbd2
K D <kreutz@acm.org>
2019-02-20 21:18:51 +0100

    last updates done at Wed Feb 20 21:18:51 CET 2019

---
+++ tpc_wrseg2019.php
@@ -0,0 +1,11 @@
+<li>Adriano Fiorese (UDESC)</li>
+<li>Carlos Schaeffer (UPF)</li>
+<li>Diego Kreutz (UNIPAMPA)</li>
+<li>Djamel Sadok (UFPE)</li>
+<li>Giovane Moura (SIDN)</li>
+<li>João Ceron (Universidade de Twente)</li>
+<li>Marco Trentin (UPF)</li>
+<li>Rafael Aschoff (IFPE)</li>
+<li>Reinaldo Gomes (UFCG)</li>
+<li>Ricardo Schmidt (UPF)</li>
+<li>Roberto Samarone Araújo (UFPA)</li>
