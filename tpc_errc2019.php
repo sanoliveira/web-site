1b681144f88fe7df8a9f55e8cf3baf1180d96447
K D <kreutz@acm.org>
2019-02-21 08:31:07 +0100

    last updates done at Thu Feb 21 08:31:07 CET 2019

--- tpc_errc2019.php
+++ tpc_errc2019.php
@@ -1,3 +1,16 @@
+<li>Adriano Fiorese (UDESC)</li>
+<li>Carlos Schaeffer (UPF)</li>
+<li>Diego Kreutz (UNIPAMPA)</li>
+<li>Djamel Sadok (UFPE)</li>
+<li>Felipe Nunes (AMF)</li>
+<li>Giovane Moura (SIDN)</li>
+<li>João Ceron (Universidade de Twente)</li>
+<li>Marco Trentin (UPF)</li>
+<li>Rafael Aschoff (IFPE)</li>
+<li>Reinaldo Gomes (UFCG)</li>
+<li>Ricardo Schmidt (UPF)</li>
+<li>Roberto Samarone Araújo (UFPA)</li>
+<li>Vinicius Vielmo Cogo (ULisboa)</li>
 <li>Adenauer Yamin (UFPEL)</li>
 <li>Adriano Fiorese (UDESC)</li>
 <li>Alberto Schaeffer-Filho (UFRGS)</li>
@@ -19,6 +32,7 @@
 <li>Erico Amaral (UNIPAMPA)</li>
 <li>Ewerton Salvador (UFPB)</li>
 <li>Fauzi Shubeita (SETREM)</li>
+<li>Felipe Nunes (AMF)</li>
 <li>Fábio Rossi (PUC-RS)</li>
 <li>Gerson Batistti (UNIJUI)</li>
 <li>Gerson Soares (UNIMI)</li>
@@ -60,4 +74,5 @@
 <li>Roger Immich (IC/UNICAMP)</li>
 <li>Tiago Ferreto (PUC-RS)</li>
 <li>Tiago Sanchotente (Raptor)</li>
+<li>Vinicius Vielmo Cogo (ULisboa)</li>
 <li>Vinícius Guimarães (IFSUL)</li>
