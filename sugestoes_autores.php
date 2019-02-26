d360b1177107c7d510b009aa634c0c10d70c06cc
Rodrigo Mansilha <rodrigo.mansilha@inf.ufrgs.br>
2019-02-26 10:39:23 -0300

    Update sugestoes_autores.php

--- sugestoes_autores.php
+++ sugestoes_autores.php
@@ -1,8 +1,8 @@
 <h3>Sugest&otilde;es aos Autores</h3>
 
 <p>
-A seguir são apresentadas sugest&otilde;es aos autores que desejam submeter seus
-artigos para publica&ccedil;&atilde;o e apresenta&ccedil;&atilde;o na ERRC/WRSeg 2019.
+A seguir são apresentadas sugest&otilde;es endereçadas aos autores que desejam submeter seus
+artigos para publica&ccedil;&atilde;o e apresenta&ccedil;&atilde;o na ERRC/WRSeg 2019. Cada sugestão é acompanhada de referências que podem ajudar na sua concretização.
 </p>
 
 <ul class="lista">
@@ -19,6 +19,11 @@ artigos para publica&ccedil;&atilde;o e apresenta&ccedil;&atilde;o na ERRC/WRSeg
       artigo compara o trabalho com outros na literatura; no quesito m&eacute;rito t&eacute;cnico, se o artigo
       diz claramente ao revisor qual a contribui&ccedil;&atilde;o do trabalho.
       </li>
+
+      <li>
+      Na prepara&ccedil;&atilde;o do seu artigo, use o formato exigido. O ERRC/WRSeg 2019 exige o <a href="http://www.sbc.org.br/documentos-da-sbc/category/169-templates-para-artigos-e-capitulos-de-livros">
+      modelo da SBC</a>. Atente para o espaço disponível.
+      </li>   
    </ul> 
    </li>
 
@@ -27,15 +32,21 @@ artigos para publica&ccedil;&atilde;o e apresenta&ccedil;&atilde;o na ERRC/WRSeg
    <br><br>
    <ul class="lista">
       <li>
-      Um dos melhores recursos dispon&iacute;veis em Portugu&ecirc;s &eacute; o <a href="http://www.inf.ufpr.br/pos/techreport/RT_DINF004_2004.pdf">"Pequeno Manual da Escrita T&eacute;cnica"</a>, escrito
+      Um dos melhores recursos dispon&iacute;veis em portugu&ecirc;s &eacute; o <a href="http://www.inf.ufpr.br/pos/techreport/RT_DINF004_2004.pdf">"Pequeno Manual da Escrita T&eacute;cnica"</a>, escrito
       por Roberto Hexsel. Esse material &eacute; uma boa leitura para alunos ao escreverem seus
-      trabalhos. Um dos lemas do texto &eacute; "leia o que escreveu";
+      trabalhos. Um dos lemas do texto &eacute; "leia o que escreveu".
       </li>
       
-      <li>Outro material similar é o <a href="http://1.49.d5.sl.pt/">"Como escrever um Artigo Cient&iacute;fico ou T&eacute;cnico: Alguns Aspectos"</a>
+      <li>
+      Outro bom material em português é o <a href="http://1.49.d5.sl.pt/">"Como escrever um Artigo Cient&iacute;fico ou T&eacute;cnico: Alguns Aspectos"</a>.
       </li>
       
-<li>&Eacute; importante tamb&eacute;m entender os principais aspectos elencados em <a href="https://www.scribd.com/document/11655966/Como-Nao-Escrever-um-Artigo-Cientifico">"Como (n&atilde;o) Escrever um Artigo T&eacute;cnico-Cient&iacute;fico"</a>
+      <li>
+      &Eacute; importante tamb&eacute;m entender os principais aspectos elencados em <a href="https://www.scribd.com/document/11655966/Como-Nao-Escrever-um-Artigo-Cientifico">"Como (n&atilde;o) Escrever um Artigo T&eacute;cnico-Cient&iacute;fico"</a>.
+      </li>
+
+      <li>
+      <a href="http://www.ic.unicamp.br/~rodolfo/dicas/escrita.html">"Dicas sobre escrita"</a> e <a href="http://www.ic.unicamp.br/~rodolfo/dicas/apresentacoes.html">"Dicas sobre apresenta&ccedil;&otilde;es"</a>, elaboras por Rodolfo de Azevedo, cont&eacute;m  dicas importantes e &uacute;teis para escrita e apresentação de artigos.
       </li>
 
       <li>
@@ -45,30 +56,25 @@ artigos para publica&ccedil;&atilde;o e apresenta&ccedil;&atilde;o na ERRC/WRSeg
       aspectos de estilo.
       </li> 
 
-        <li> <a href="https://www.bartleby.com/141/index.html">"The Elements of Style"</a> &eacute; uma refer&ecirc;ncia interessante para compreender e aplicar elementos de estilo na escrita.
+      <li>
+      <a href="https://www.bartleby.com/141/index.html">"The Elements of Style"</a> &eacute; uma refer&ecirc;ncia interessante para compreender e aplicar elementos de estilo na escrita.
       </li> 
 
-      <li> A Introdu&ccedil;&atilde;o
-      (<a href="http://www.cs.columbia.edu/~hgs/etc/intro-style.html">"Writing a good introduction"</a>) é fundamental para ``converter'' um potencial leitor em um leitor de fato.
-      </li>
-      
       <li>
-<a href="http://www.ic.unicamp.br/~rodolfo/dicas/escrita.html">"Dicas sobre escrita"</a> e <a href="http://www.ic.unicamp.br/~rodolfo/dicas/apresentacoes.html">"Dicas sobre
-apresenta&ccedil;&otilde;es"</a>, elaboras por
-Rodolfo de Azevedo, cont&eacute;m  dicas importantes e &uacute;teis para escrita e apresentação de artigos.
+      A Introdu&ccedil;&atilde;o (<a href="http://www.cs.columbia.edu/~hgs/etc/intro-style.html">"Writing a good introduction"</a>) é fundamental para ``converter'' um potencial leitor, que se interessou pela introdução, em um leitor do trabalho completo.
       </li>
       
       <!--
       <li>
       H&aacute; uma p&aacute;gina com dicas gramaticais aqui, e recomendo como material de refer&ecirc;ncia em rela&ccedil;&atilde;o ao 
       Portugu&ecirc;s, consulte aqui o manual de escrita t&eacute;cnica, na PUCRS.
-
       </li>
---> 
+      --> 
 
-       <li>
-        "On Writing Well", escrito por William Zinsser, &eacute; um dos melhores e mais sucintos livros sobre como escrever bem. Usar palavras simples. Frases curtas e objetivas. Entre tantas outras coisas. Apesar de ser um livro voltado para escrita em ingl&ecirc;s, boa parte de suas dicas podem ser aplicadas em outras l&iacute;nguas, como portugu&ecirc;s.
+      <li>
+      "On Writing Well", escrito por William Zinsser, &eacute; um dos melhores e mais sucintos livros sobre como escrever bem.  Apesar de ser um livro voltado para escrita em ingl&ecirc;s, boa parte de suas dicas podem ser aplicadas em outras l&iacute;nguas, como portugu&ecirc;s. Uma  dica apresentada é dar preferência para palavras simples, frases curtas e objetivas.
       </li>
+
       <li>
       O post <a href="https://goo.gl/s9AeLs">"The Six Best Tips From On Writing Well"</a> sumariza as principais
       dicas do livro "On Writing Well".
@@ -93,22 +99,15 @@ Rodolfo de Azevedo, cont&eacute;m  dicas importantes e &uacute;teis para escrita
       pe&ccedil;a para amigos ou profissionais em revis&atilde;o de texto dar um toque de qualidade ao seu texto. 
       </li>
 
-      <li>Trocar a cor da fonte ou o meio (digital/papel) pode ajudar melhorar a concentração em trechos previamente revisados.
+      <li>Trocar a cor da fonte ou o meio (eletrônico/impresso) pode ajudar recobrar a concentração em trechos que já tenham sido revisados múltiplas vezes anteriormente.
       </li>
 
       <li>
-      Faça uma revisão adequada da forma do artigo. Nesta estapa, atente para os aspectos como uso da v&iacute;rgula, crase e concord&acirc;ncia nominal.
-      Verifique tamb&eacute;m se as refer&ecirc;ncias a figuras, tabelas, equa&ccedil;&otilde;es, etc. est&atilde;o corretas, se cita&ccedil;&otilde;es
-      bibliogr&aacute;ficas constam corretamente como tamb&eacute;m se seus conte&uacute;dos procedem ao referenciado no
-      artigo. 
+      Faça um checklist sobre a formatação do artigo. Nesta estapa, use um corretor ortográfico para evitar erros básicos, como uso da v&iacute;rgula, crase e concord&acirc;ncia nominal. Em seguida, verifique se todos elementos auxiliares, como figuras, tabelas, equações, são referenciados no texto e de maneira adequada. Para cada citação, verifique: se (i) ela é pertinente para trabalho, (ii) se ela é coerente com o artigo referenciado e não é uma referência indireta, e (iii) aponta para o artigo correto. Verifique se todas as referência seguem mesmo padrão, por exemplo, quanto ao grau de detalhes. 
       </li>
 
       <li>
-      Na prepara&ccedil;&atilde;o do seu artigo, use o formato exigido, conforme o <a href="http://www.sbc.org.br/documentos-da-sbc/category/169-templates-para-artigos-e-capitulos-de-livros">
-      modelo da SBC aqui.</a>
-      </li>   
-
-      <li>Faça uma revisão fim a fim de uma versão impressa evitar problemas de impressão.
+      Por fim, imprima o artigo, idealmente em uma via colorida e outra preto e branco, e procure detectar possíveis problemas de impressão.
       </li>
    </ul>
    </li>
