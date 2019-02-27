0869f773f99e1b0710e203449e3dde26be273971
Rodrigo Mansilha <rodrigo.mansilha@inf.ufrgs.br>
2019-02-26 21:26:59 -0300

    formatacao

--- sugestoes_autores.php
+++ sugestoes_autores.php
@@ -6,62 +6,61 @@ artigos para publica&ccedil;&atilde;o e apresenta&ccedil;&atilde;o na ERRC/WRSeg
 </p>
 
 <ul class="lista">
+   <br>
    <li>
-   A primeira sugestão é planejar o que será escrito antes de começar a escrever. 
+   A primeira sugestão é <b>planejar</b> o que será escrito antes de começar a escrever. 
    <br><br>
    <ul class="lista">
-      <li>Dicas gerais são apresentadas em <a href="http://blizzard.cs.uwaterloo.ca/keshav/home/Papers/data/10/ogres2.pdf">``Ogres, Villagers, & the Art of Writing
-   ''</a>, escrito por S. Keshav.
+      <li class="text-justify">Dicas gerais são apresentadas em <a href="http://blizzard.cs.uwaterloo.ca/keshav/home/Papers/data/10/ogres2.pdf">&ldquo;Ogres, Villagers, & the Art of Writing&rdquo;</a>, escrito por S. Keshav.
       </li>
 
-      <li>Para planejar aspectos mais específicos do artigo a ser submetido para a ERRC, pode ser útil analisar o formulário que será empregado pelos revisores.
+      <li class="text-justify">
+      Para planejar aspectos mais específicos do artigo a ser submetido para a ERRC, pode ser útil analisar o formulário que será empregado pelos revisores.
       Use o formulário como guia para auto-avaliar o seu artigo. Verifique, por exemplo, no quesito originalidade, se o seu
       artigo compara o trabalho com outros na literatura; no quesito m&eacute;rito t&eacute;cnico, se o artigo
       diz claramente ao revisor qual a contribui&ccedil;&atilde;o do trabalho.
       </li>
 
-      <li>
-      Na prepara&ccedil;&atilde;o do seu artigo, use o formato exigido. O ERRC/WRSeg 2019 exige o <a href="http://www.sbc.org.br/documentos-da-sbc/category/169-templates-para-artigos-e-capitulos-de-livros">
-      modelo da SBC</a>. Atente para o espaço disponível.
+      <li class="text-justify">
+      Na prepara&ccedil;&atilde;o do seu artigo, use o formato exigido. O ERRC/WRSeg 2019 exige o <a href="http://www.sbc.org.br/documentos-da-sbc/category/169-templates-para-artigos-e-capitulos-de-livros">modelo da SBC</a>. Atente para o espaço disponível.
       </li>   
    </ul> 
    </li>
 
+   <br>
    <li>
-   A segunda sugestão é estudar o processo de escrita. 
+   A segunda sugestão é <b>estudar</b> o processo de escrita. 
    <br><br>
    <ul class="lista">
-      <li>
-      Um dos melhores recursos dispon&iacute;veis em portugu&ecirc;s &eacute; o <a href="http://www.inf.ufpr.br/pos/techreport/RT_DINF004_2004.pdf">"Pequeno Manual da Escrita T&eacute;cnica"</a>, escrito
+      <li class="text-justify">
+      Um dos melhores recursos dispon&iacute;veis em portugu&ecirc;s &eacute; o <a href="http://www.inf.ufpr.br/pos/techreport/RT_DINF004_2004.pdf">&ldquo;Pequeno Manual da Escrita T&eacute;cnica&rdquo;</a>, escrito
       por Roberto Hexsel. Esse material &eacute; uma boa leitura para alunos ao escreverem seus
-      trabalhos. Um dos lemas do texto &eacute; "leia o que escreveu".
+      trabalhos. Um dos lemas do texto &eacute; &ldquo;leia o que escreveu&rdquo;.
       </li>
       
-      <li>
-      Outro bom material em português é o <a href="http://1.49.d5.sl.pt/">"Como escrever um Artigo Cient&iacute;fico ou T&eacute;cnico: Alguns Aspectos"</a>.
+      <li class="text-justify">
+      Outro bom material em português é o <a href="http://1.49.d5.sl.pt/">&ldquo;Como escrever um Artigo Cient&iacute;fico ou T&eacute;cnico: Alguns Aspectos&rdquo;</a>.
       </li>
       
-      <li>
-      &Eacute; importante tamb&eacute;m entender os principais aspectos elencados em <a href="https://www.scribd.com/document/11655966/Como-Nao-Escrever-um-Artigo-Cientifico">"Como (n&atilde;o) Escrever um Artigo T&eacute;cnico-Cient&iacute;fico"</a>.
+      <li class="text-justify">
+      &Eacute; importante tamb&eacute;m entender os principais aspectos elencados em <a href="https://www.scribd.com/document/11655966/Como-Nao-Escrever-um-Artigo-Cientifico">&ldquo;Como (n&atilde;o) Escrever um Artigo T&eacute;cnico-Cient&iacute;fico&rdquo;</a>.
       </li>
 
-      <li>
-      <a href="http://www.ic.unicamp.br/~rodolfo/dicas/escrita.html">"Dicas sobre escrita"</a> e <a href="http://www.ic.unicamp.br/~rodolfo/dicas/apresentacoes.html">"Dicas sobre apresenta&ccedil;&otilde;es"</a>, elaboras por Rodolfo de Azevedo, cont&eacute;m  dicas importantes e &uacute;teis para escrita e apresentação de artigos.
+      <li class="text-justify">
+      <a href="http://www.ic.unicamp.br/~rodolfo/dicas/escrita.html">&ldquo;Dicas sobre escrita&rdquo;</a> e <a href="http://www.ic.unicamp.br/~rodolfo/dicas/apresentacoes.html">&ldquo;Dicas sobre apresenta&ccedil;&otilde;es&rdquo;</a>, elaboras por Rodolfo de Azevedo, cont&eacute;m  dicas importantes e &uacute;teis para escrita e apresentação de artigos.
       </li>
 
-      <li>
-      <a href="http://www.cs.columbia.edu/~hgs/etc/writing-style.html">"Writing Technical Articles"</a>,
-      escrito por Henning Schulzrinne, descreve em detalhes
-      o processo de se elaborar um artigo t&eacute;cnico de qualidade na &aacute;rea da computa&ccedil;&atilde;o, tratando tamb&eacute;m
-      aspectos de estilo.
+      <li class="text-justify">
+      <a href="http://www.cs.columbia.edu/~hgs/etc/writing-style.html">&ldquo;Writing Technical Articles&rdquo;</a>,
+      escrito por Henning Schulzrinne, descreve em detalhes o processo de se elaborar um artigo t&eacute;cnico de qualidade na &aacute;rea da computa&ccedil;&atilde;o, tratando tamb&eacute;m aspectos de estilo.
       </li> 
 
-      <li>
-      <a href="https://www.bartleby.com/141/index.html">"The Elements of Style"</a> &eacute; uma refer&ecirc;ncia interessante para compreender e aplicar elementos de estilo na escrita.
+      <li class="text-justify">
+      <a href="https://www.bartleby.com/141/index.html">&ldquo;The Elements of Style&rdquo;</a> &eacute; uma refer&ecirc;ncia interessante para compreender e aplicar elementos de estilo na escrita.
       </li> 
 
-      <li>
-      A Introdu&ccedil;&atilde;o (<a href="http://www.cs.columbia.edu/~hgs/etc/intro-style.html">"Writing a good introduction"</a>) é fundamental para ``converter'' um potencial leitor, que se interessou pela introdução, em um leitor do trabalho completo.
+      <li class="text-justify">
+      A Introdu&ccedil;&atilde;o (<a href="http://www.cs.columbia.edu/~hgs/etc/intro-style.html">&ldquo;Writing a good introduction&rdquo;</a>) é fundamental para ``converter'' um potencial leitor, que se interessou pela introdução, em um leitor do trabalho completo.
       </li>
       
       <!--
@@ -71,43 +70,45 @@ artigos para publica&ccedil;&atilde;o e apresenta&ccedil;&atilde;o na ERRC/WRSeg
       </li>
       --> 
 
-      <li>
-      "On Writing Well", escrito por William Zinsser, &eacute; um dos melhores e mais sucintos livros sobre como escrever bem.  Apesar de ser um livro voltado para escrita em ingl&ecirc;s, boa parte de suas dicas podem ser aplicadas em outras l&iacute;nguas, como portugu&ecirc;s. Uma  dica apresentada é dar preferência para palavras simples, frases curtas e objetivas.
+      <li class="text-justify">
+      &ldquo;On Writing Well&rdquo;, escrito por William Zinsser, &eacute; um dos melhores e mais sucintos livros sobre como escrever bem.  Apesar de ser um livro voltado para escrita em ingl&ecirc;s, boa parte de suas dicas podem ser aplicadas em outras l&iacute;nguas, como portugu&ecirc;s. Uma  dica apresentada é dar preferência para palavras simples, frases curtas e objetivas.
       </li>
 
-      <li>
-      O post <a href="https://goo.gl/s9AeLs">"The Six Best Tips From On Writing Well"</a> sumariza as principais
+      <li class="text-justify">
+      O post <a href="https://goo.gl/s9AeLs">&ldquo;The Six Best Tips From On Writing Well&rdquo;</a> sumariza as principais
       dicas do livro "On Writing Well".
       </li>
       
-      <li>
-      <a href="https://sivers.org/book/OnWritingWell">"As notas de Derek Sivers"</a>, do livro "On Writing Well",
+      <li class="text-justify">
+      <a href="https://sivers.org/book/OnWritingWell">&ldquo;As notas de Derek Sivers&rdquo;</a>, do livro &ldquo;On Writing Well&rdquo;,
       cont&eacute;m v&aacute;rias dicas para melhorar substancialmente a habilidade de escrita de qualquer pessoa.
       Tente seguir as dicas para obter resultados cada vez melhores.
       </li>
    </ul>
    </li>
    
+   <br>
    <li>
-   A terceira sugestão é revisar o processo de escrita. 
+   A terceira sugestão é <b>revisar</b> o artigo. 
    <br><br>
    <ul class="lista">
 
-      <li>
+      <li class="text-justify">
       Fa&ccedil;a uma revis&atilde;o adequada do conteúdo do artigo antes de submeter. Pe&ccedil;a para seus 
       co-autores mais experientes em escrita revisar cuidadosamente a vers&atilde;o final. Alternativamente, 
       pe&ccedil;a para amigos ou profissionais em revis&atilde;o de texto dar um toque de qualidade ao seu texto. 
       </li>
 
-      <li>Trocar a cor da fonte ou o meio (eletrônico/impresso) pode ajudar recobrar a concentração em trechos que já tenham sido revisados múltiplas vezes anteriormente.
+      <li class="text-justify">
+      Trocar a cor da fonte ou o meio (eletrônico/impresso) pode ajudar recobrar a concentração em trechos que já tenham sido revisados múltiplas vezes anteriormente.
       </li>
 
-      <li>
+      <li class="text-justify">
       Faça um checklist sobre a formatação do artigo. Nesta estapa, use um corretor ortográfico para evitar erros básicos, como uso da v&iacute;rgula, crase e concord&acirc;ncia nominal. Em seguida, verifique se todos elementos auxiliares, como figuras, tabelas, equações, são referenciados no texto e de maneira adequada. Para cada citação, verifique: se (i) ela é pertinente para trabalho, (ii) se ela é coerente com o artigo referenciado e não é uma referência indireta, e (iii) aponta para o artigo correto. Verifique se todas as referência seguem mesmo padrão, por exemplo, quanto ao grau de detalhes. 
       </li>
 
-      <li>
-      Por fim, imprima o artigo, idealmente em uma via colorida e outra preto e branco, e procure detectar possíveis problemas de impressão.
+      <li class="text-justify">
+      Por fim, imprima duas vias do artigo, uma colorida e outra preto e branco, e procure detectar possíveis problemas de impressão.
       </li>
    </ul>
    </li>
