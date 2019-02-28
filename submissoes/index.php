d319e30be2ad6ee3e90f5e891419de1061b9deba
K D <kreutz@acm.org>
2019-02-28 10:26:45 +0100

    last updates done at Thu Feb 28 10:26:45 CET 2019

--- submissoes/index.php
+++ submissoes/index.php
@@ -1,1 +1,1 @@
-<meta http-equiv="refresh" content="0; url=https://jems.sbc.org.br/jems2/index.php?r=conference/main&c=3279" />
+<meta http-equiv="refresh" content="0; url=https://submissoes.sbc.org.br/home.cgi?c=3279" />
