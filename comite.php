03bbc667a7feff5aaa22ccc24b69e8d235909393
K D <kreutz@acm.org>
2019-03-02 10:13:52 +0100

    last updates done at Sat Mar  2 10:13:51 CET 2019

---
+++ comite.php
@@ -0,0 +1,17 @@
+<?php
+    require_once('topo.php');
+?>
+
+<div class="container-fluid bg-success">
+    <div class="container">
+        <h3>Comit&ecirc; de Programa (em forma&ccedil;&atilde;o)</h3>
+        <ul class="lista">
+            <?php include 'tpc_errc2019.php';?>
+        </ul>
+        <br><br>
+    </div>
+</div><!-- fim comite de programa -->
+
+<?php
+    require_once('rodape.php');
+?>
---
+++ wrseg/chamada/index.php
@@ -0,0 +1,1 @@
+<meta http-equiv="refresh" content="0; url=https://errc.sbc.org.br/2019/wrseg.php" />
