a5ea934fb4ac41e5898f16cf05928ab33781c29e
K D <kreutz@acm.org>
2019-03-02 11:18:50 +0100

    last updates done at Sat Mar  2 11:18:50 CET 2019

---
+++ chamada_de_trabalhos.php
@@ -0,0 +1,37 @@
+
+<h3>Chamada para Trabalhos</h3>
+
+<p>Autores s&atilde;o convidados a submeter artigos e resumos contendo
+resultados de suas pesquisas cient&iacute;ficas ou
+tecnol&oacute;gicas. Cada trabalho ser&aacute; avaliado por
+tr&ecirc;s ou mais especialistas.
+<p>A edi&ccedil;&atilde;o de 2019 contemplará duas <b>trilhas</b> de trabalhos:</p>
+
+<ul class="lista">
+<li><b>Regular</b>: trabalhos cient&iacute;ficos tradicionais, oriundos de pesquisa formal.</li>
+
+
+<li><b>Experi&ecirc;ncias</b>: trabalhos que descrevem experi&ecirc;ncias pr&aacute;ticas com t&eacute;cnicas ou tecnologias atuais e de interesse do p&uacute;blico alvo do evento. Exemplo de redes: implanta&ccedil;&atilde;o de uma rede definida por software (SDN) em uma rede corporativa. Exemplo de seguran&ccedil;a: implanta&ccedil;&atilde;o da tecnologia Intel SGX para autentica&ccedil;&atilde;o de usu&aacute;rios.</li>
+</ul>
+<br>
+
+<p><b>Datas Importantes</b></p>
+
+<ul class="lista">
+
+<li><b>10/julho/2019</b>: data <b>limite</b> para registro de trabalho</li>
+
+<li><b>15/julho/2019</b>: data <b>limite</b> para submiss&atilde;o dos trabalhos</li>
+
+<li>02/agosto/2019: notifica&ccedil;&atilde;o aos autores</li>
+
+<li>15/agosto/2019: entrega da vers&atilde;o final dos trabalhos aceitos</li>
+
+<li>16-19/setembro/2019: realiza&ccedil;&atilde;o do evento</li>
+
+</ul><!-- fim lista -->
+<br>
+
+
+<p>Na se&ccedil;&atilde;o de submiss&atilde;o de trabalhos (a seguir), os autores ir&atilde;o encontrar mais informa&ccedil;&atilde;o sobre as modalidades de trabalhos e a forma de envio.</p>
+        
