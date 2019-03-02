b316cd64513a1450f3961189f5207ab00e27b708
K D <kreutz@acm.org>
2019-03-02 11:41:09 +0100

    last updates done at Sat Mar  2 11:41:09 CET 2019

--- wrseg/submissoes/index.php
+++ wrseg/submissoes/index.php
@@ -1,6 +1,3 @@
 
-<?php
-    require_once('disponivel_em_breve.php');
-?>
-
 <!-- <meta http-equiv="refresh" content="0; url=https://errc.sbc.org.br/2019/" /> --!>
+<meta http-equiv="refresh" content="0; url=https://errc.sbc.org.br/2019/disponivel_em_breve.php" />
