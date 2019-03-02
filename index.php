a485ed969c02e23196ab819937e3ed7a5ef58480
Rodrigo Mansilha <rodrigo.mansilha@inf.ufrgs.br>
2019-03-02 15:33:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -33,8 +33,7 @@
                 <h4>Calendário</h4> 
                 <!-- 1:1 aspect ratio -->
                 <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=2019errc%40gmail.com&amp;color=%23125A12&amp;ctz=America%2FSao_Paulo" width="400" height="300"></iframe>
-                    </iframe>
+                    <iframe class="embed-responsive-item" src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=2019errc%40gmail.com&amp;color=%23125A12&amp;ctz=America%2FSao_Paulo" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
             </div>
         </div> <!-- fim row -->
     </div> <!-- fim container -->
