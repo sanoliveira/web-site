7a3ef30533bdc6656fca18f23e0d1d68bb49d1cb
K D <kreutz@acm.org>
2019-03-02 11:42:28 +0100

    last updates done at Sat Mar  2 11:42:28 CET 2019

--- disponivel_em_breve.php
+++ disponivel_em_breve.php
@@ -5,7 +5,7 @@
 <div class="container-fluid bg-success">
     <div class="container">
         <!-- INICIO CONTEUDO INSCRIÇÕES      -->
-        <br><br><br>
+        <br><br><br><br><br>
         <h3>Submiss&atilde;o de Trabalhos</h3>
         <p>
             Em breve ser&aacute; disponibilizado o link para submiss&atilde;o de trabalhos.
