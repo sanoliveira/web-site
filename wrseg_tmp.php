a644354fdc3fff7852d48aea806188785c85e2b6
K D <kreutz@acm.org>
2019-03-28 10:46:22 -0300

    last updates done at Thu Mar 28 10:46:22 -03 2019

---
+++ wrseg_tmp.php
@@ -0,0 +1,31 @@
+<?php
+    require_once('topo.php');
+?>
+
+<div class="container-fluid bg-success">
+    <div class="container">
+        <!--         INICIO CONTEUDO WRSEG      -->
+        <br><br><br>
+        <h3>
+            <center>
+            4&ordm; Workshop Regional de Seguran&ccedil;a da
+            Informa&ccedil;&atilde;o e de Sistemas Computacionais
+            <br>
+            WRSeg 2019
+            </center>
+        </h3>
+
+        <p>
+        Em breve maiores informações!
+        </p>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="container">
+
+
+
+<?php
+    require_once('rodape.php');
+?>
