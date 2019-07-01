ee77826882ec050f298407b12787329cbce4b694
Rodrigo Mansilha <rodrigo.mansilha@inf.ufrgs.br>
2019-07-01 14:33:03 -0300

    CNPq

--- apoio.php
+++ apoio.php
@@ -83,32 +83,36 @@
         <div class="col-lg-5 col-md-5 text-center">
             
             <h4>Patrocínio Diamante</h4>
-        
+    
                 <a href="https://www.sicredi.com.br" target="_blank" title="Sicredi">
                     <img class="patrocinadora-diamante" alt="Sicredi" src="img/logo_inst_patrocinadora/sicredi_horizontal.png">
+                </a>
+            
+                <a href="http://www.cnpq.br" target="_blank" title="CNPq">
+                    <img class="patrocinadora-diamante" alt="CNPq" src="img/logo_inst_patrocinadora/cnpq.png">
                 </a>
         
             <h4>Patrocínio Ouro</h4>
                 <a href="http://www.conesul.net" target="_blank" title="RedeConesul Telecom">
                     <img class="patrocinadora-ouro" alt="RedeConesul Telecom" src="img/logo_inst_patrocinadora/conesul-marca-errc-01.png">
                 </a>
-            <a href="https://www.interneith.com.br/" target="_blank" title="Interneith">
+            
+                <a href="https://www.interneith.com.br/" target="_blank" title="Interneith">
                     <img class="patrocinadora-ouro" alt="Interneith" src="img/logo_inst_patrocinadora/interneith.png">
-            </a>
-            <a href="https://www.avato.com.br/" target="_blank" title="Ávato">
+                </a>
+                
+                <a href="https://www.avato.com.br/" target="_blank" title="Ávato">
                     <img class="patrocinadora-ouro" alt="Ávato" src="img/logo_inst_patrocinadora/avato_logo_AZ.png">
-            </a>
+                </a>
             
             <h4>Patrocínio Prata</h4>
-            <a href="https://www.facebook.com/acessoalegrete/" target="_blank" title="Acesso Informática">
+                <a href="https://www.facebook.com/acessoalegrete/" target="_blank" title="Acesso Informática">
                     <img class="patrocinadora-prata" alt="Acesso" src="img/logo_inst_patrocinadora/Acesso-logo.jpg">
-            </a>
-            <a href="https://www.trendmicro.com/" target="_blank" title="Trend Micro">
+                </a>
+            
+                <a href="https://www.trendmicro.com/" target="_blank" title="Trend Micro">
                     <img class="patrocinadora-prata" alt="Trend Micro" src="img/logo_inst_patrocinadora/TM_logo_tagline_2018_horiz_2c.png">
-            </a>
-
-           
-
+                </a>
 
             <h4>Patrocínio Bronze</h4>
             <a href="https://www.parceriaarmas.com.br/clube-de-tiro/" target="_blank" title="Parceria Clube de Tiro">
