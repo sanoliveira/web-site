2e2d4815c057dd8e61728d2a8c45d56323a115af
K D <kreutz@acm.org>
2019-07-21 13:39:35 -0300

    last updates done at Sun Jul 21 13:39:35 -03 2019

--- datas_importantes.php
+++ datas_importantes.php
@@ -2,13 +2,13 @@
     <h4>Datas Importantes (<font color="red"> <b>** NOVAS DATAS **</b> </font>)</h4>
     <br>
     <ul class="lista">
-        <li><font color="red"><b>25/julho/2019</b></font>: data <b>limite</b> para registro de trabalho</li>
+        <li><font color="red"><b>28/julho/2019</b></font>: data <b>limite</b> para registro de trabalho</li>
 
-        <li><font color="red"><b>25/julho/2019</b></font>: data <b>limite</b> para submiss&atilde;o dos trabalhos</li>
+        <li><font color="red"><b>28/julho/2019</b></font>: data <b>limite</b> para submiss&atilde;o dos trabalhos</li>
 
-        <li>10/agosto/2019: notifica&ccedil;&atilde;o aos autores</li>
+        <li>16/agosto/2019: notifica&ccedil;&atilde;o aos autores</li>
 
-        <li>20/agosto/2019: entrega da vers&atilde;o final dos trabalhos aceitos</li>
+        <li>26/agosto/2019: entrega da vers&atilde;o final dos trabalhos aceitos</li>
         <li>16-19/setembro/2019: realiza&ccedil;&atilde;o do evento</li>
     </ul><!-- fim lista -->
 <!-- FIM DATAS IMPORTANTES -->
