fc57558649c50ac17dd8441b6e88c205ed052d8b
K D <kreutz@acm.org>
2019-07-31 18:47:39 -0300

    last updates done at Wed Jul 31 18:47:39 -03 2019

--- tpc_wrseg2019.php
+++ tpc_wrseg2019.php
@@ -1,30 +1,17 @@
-<li>Adenauer Correa Yamin (UCPEL/UFPEL)</li>
-<li>Adriane Ramires (IFSul-Pelotas)</li>
-<li>Adriano Fiorese (UDESC)</li>
-<li>Avelino F. Zorzo (PUCRS)</li>
+<li>Adenauer Yamin (UFPEL)</li>
 <li>Bruno Dalmazo (UFRGS)</li>
 <li>Bruno Mozzaquatro (UFSM)</li>
-<li>Carlos Schaeffer (UPF)</li>
 <li>Carlos Vinicius Rasch Alves (SENAC-RS)</li>
 <li>Charles Christian Miers (UDESC)</li>
-<li>Cristiano Bonato Both (UNISINOS)</li>
 <li>Diego Kreutz (UNIPAMPA)</li>
 <li>Diogo Menezes Ferrazani Mattos (UFF)</li>
-<li>Djamel Sadok (UFPE)</li>
-<li>Douglas Macedo (UFSC)</li>
 <li>Eduardo Monks (SENAC-RS)</li>
 <li>Erico Hoff do Amaral (UNIPAMPA)</li>
 <li>Felipe Nunes (AMF)</li>
-<li>Giovane Moura (SIDN)</li>
-<li>João Ceron (Universidade de Twente)</li>
-<li>Jéferson Campos Nobre (UNISINOS)</li>
-<li>Luciano Gaspary (UFRGS)</li>
 <li>Luciano Ignaczak (UNISINOS)</li>
 <li>Luis Augusto Dias Knob (IFRS-Sertão)</li>
 <li>Marco Antônio Sandini Trentin (UPF)</li>
 <li>Márcia Henke (UFSM)</li>
-<li>Rafael Aschoff (IFPE)</li>
-<li>Raul Ceretta Nunes (UFSM)</li>
 <li>Reinaldo Gomes (UFCG)</li>
 <li>Ricardo Almeida (UFPEL)</li>
 <li>Ricardo Schmidt (UPF)</li>
@@ -32,7 +19,5 @@
 <li>Roberto Samarone Araújo (UFPA)</li>
 <li>Rodrigo Mansilha (UNIPAMPA)</li>
 <li>Rogério Turchetti (UFSM)</li>
-<li>Simone Ceolin (UFSM)</li>
 <li>Tiago Antônio Rizzetti (UFSM)</li>
-<li>Vinicius Vielmo Cogo (ULisboa)</li>
-<li>Weverton Cordeiro (UFRGS)</li>
+<li>Walter Priesnitz Filho (UFSM)</li>
