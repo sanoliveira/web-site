e80b95a75142ae43f58f1bc63f2e58be96cc7017
Rodrigo Mansilha <rodrigo.mansilha@inf.ufrgs.br>
2019-08-05 18:20:26 -0300

    Inscricoes

--- inscricoes.php
+++ inscricoes.php
@@ -1,16 +1,144 @@
 <?php
     require_once('topo.php');
 ?>
+<!-- INICIO CONTEUDO INSCRIÇÕES      -->
+<a class="anchor" id="inscricoes"></a>
+<div class="container-fluid ">
+    <div class="container">
+        
+        <br><br><br>
+        <h3>Inscri&ccedil;&otilde;es</h3>
+        <p>Estão abertas as inscrições pelo <a href="https://centraldesistemas.sbc.org.br/ecos/errc2019">formulário de  inscrição on-line</a> da SBC. 
+        </p>
+        <br><br>
+        
+    
+          <h4>Informações Gerais</h4>
+          <ul>
+            <ul>
+              <li>Após 11/09/2019, as inscrições só poderão ser PAGAS somente no local do evento.</li>
+              <li>Os sócios da SBC devem estar em dia com a  anuidade de 2019 para terem direito aos valores reduzidos.</li>
+              <li>Pagamento da inscrição poderá ser realizado por  meio de boleto bancário, cartão de crédito VISA, débito em conta do Banco do Brasil ou nota de empenho, através do portal.</li>
+              <li>Caravanas de Estudantes: há uma promoção  especial para estudantes nesta página.</li>
+            
+                <li>A taxa de Inscrição  cobre a participação nas sessões técnicas, palestras, minicursos, material da Escola e coffee-breaks.</li>
+                
+                 
+              </ul>
+            <p>&nbsp;</p>
+          </ul>
+
+
+          <h4>Investimento </h4>
+           
+              <table align="center" width="571" border="1" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td width="241" align="center"><div align="center"><strong>Categorias</strong></div></td>
+                  <td width="162" align="center" valign="top"><div align="center"><strong>Até o dia  02/09/2019</strong></div></td>
+                  <td width="160" align="center"><div align="center"><strong>Até o dia 19/09/2019</strong></div></td>
+                  </tr>
+                  <tr>
+                  <td width="241"><div align="center">Estudante associado à SBC</div></td>
+                  <td width="162"><div align="center">R$ 9,00</div></td>
+                  <td width="160"><div align="center">R$ 29,00</div></td>
+                  </tr>
+                <tr>
+                  <td width="241"> <div align="center">Estudante não associado à SBC (inscrição + associação)</div></td>
+                  <td width="162"><div align="center">R$ 30,00</div></td>
+                  <td width="160"><div align="center">R$ 50,00</div></td>
+                  </tr>
+                <tr>
+                  <td width="241"> <div align="center">Estudante não associado à SBC (somente inscrição)</div></td>
+                  <td width="162"><div align="center">R$ 31,00</div></td>
+                  <td width="160"><div align="center">R$ 53,00</div></td>
+                  </tr>
+                <tr>
+                  <td width="241"> <div align="center">Estudante de pós-graduação associado à SBC</div></td>
+                  <td width="162"><div align="center">R$ 20,00</div></td>
+                  <td width="160"><div align="center">R$ 55,00</div></td>
+                  </tr>
+                <tr>
+                  <td width="241"> <div align="center">Estudante de pós-graduação não associado à SBC (inscrição + associação)</div></td>
+                  <td width="162"><div align="center">R$ 105,00</div></td>
+                  <td width="160"><div align="center">R$ 140,00</div></td>
+                  </tr>
+                <tr>
+                  <td width="241"> <div align="center">Estudante de pós-graduação não associado à SBC (somente inscrição)</div></td>
+                  <td width="162"><div align="center">R$ 40,00</div></td>
+                  <td width="160"><div align="center">R$ 80,00</div></td>
+                  </tr>
+                <tr>
+                    
+                  <td width="241"> <div align="center">Profissional associado à SBC</div></td>
+                  <td width="162"><div align="center">R$  40,00</div></td>
+                  <td width="160"><div align="center">R$  80,00</div></td>
+                  </tr>
+                <tr>
+                  <td width="241"> <div align="center">Profissional não associado à SBC (inscrição + associação)</div></td>
+                  <td width="162"><div align="center">R$ 280,00</div></td>
+                  <td width="160"><div align="center">R$ 320,00</div></td>
+                  </tr>
+                <tr>
+                  <td width="241"> <div align="center">Profissional não associado à SBC (somente inscrição)</div></td>
+                  <td width="162"><div align="center">R$  60,00</div></td>
+                  <td width="160"><div align="center">R$ 120,00</div></td>
+                </tr>
+                  
+                  <tr>
+                  <td width="241"> <div align="center">Estudante de Ensino Médio</div></td>
+                  <td width="162"><div align="center">R$  20,00</div></td>
+                  <td width="160"><div align="center">R$  40,00</div></td>
+                    </tr>
+                  
+                    <td width="241"> <div align="center">Professor de Ensino Médio</div></td>
+                  <td width="162"><div align="center">R$  40,00</div></td>
+                  <td width="160"><div align="center">R$  80,00</div></td>
+                </tr>
+                  
+              </table>
+             <br><br><br>
+    </div> 
+</div>  <!-- FIM CONTEUDO GERAL -->
+
+<div class="container-fluid bg-success"> <!-- CONTEUDO CARAVANAS -->
+    <div class="container">
+      <h3><a name="caravana"></a>Descontos para Caravanas</h3>
+      <p>A organização do ERRC concederá descontos especiais para  caravanas de estudantes. A cada <font color="red"><b>oito (8)</b></font> estudantes inscritos e pagos, será  concedida uma nova inscrição de estudante gratuita. </p>
+     
+      <p>Os estudantes que forem participar da caravana devem preencher o formulário de inscrição on-line, confirmar e pagar suas inscrições. Os inscritos que terão o benefício da gratuidade também devem preencher o  formulário de inscrição, porém não devem efetuar o pagamento.<br>
+        </p>
+        
+        <p>
+        Completadas as inscrições, o coordenador da caravana deverá  enviar para <a href="mailto:errc2019@unipampa.edu.br">errc2019@unipampa.edu.br</a> uma mensagem com o  assunto &ldquo;Caravana [nome da sua instituição]&ldquo;, contendo no corpo da mensagem a  relação com os nomes dos estudantes que fazem parte do grupo e indicando o(s)  nome(s) do(s) estudante(s) com direito a gratuidade.
+        </p>
+
+    </div>
+</div> <!-- FIM CONTEUDO CARAVANAS -->
+     
+
+<div class="container-fluid ">
+    <div class="container">
+        <h3>Política para autores</h3>
+
+        <p>Informamos aos autores que, para garantir a publicação de trabalhos aceitos nos anais da ERRC 2019, pelo menos um dos autores deve estar com inscrição confirmada e paga. Além disso, é necessária a assinatura e o envio do Formulário de Cessão de Direitos Autorais da Sociedade Brasileira de Computação (SBC). Pedimos que entre em contato com o(s) coordenador(es) da atividade na qual o trabalho foi aceito para instruções específicas.</p>
+
+        <p> Ressaltamos também que a apresentação do trabalho no evento (por exemplo, para artigos da trilha principal, no momento da sua respectiva sessão técnica) é obrigatória, sob risco do trabalho ser excluído dos anais do evento e do Portal de Conteúdos da SBC caso não seja apresentado.</p>
+       </div>
+</div> <!-- FIM AUTORES -->
+        
+
 
 <div class="container-fluid bg-success">
     <div class="container">
-        <!-- INICIO CONTEUDO INSCRIÇÕES      -->
-        <br><br><br>
-        <h3>Inscri&ccedil;&otilde;es</h3>
-        <p>
-            Em breve ser&atilde;o publicadas informa&ccedil;&otilde;es sobre per&iacute;odo de inscri&ccedil;&otilde;es, valores e formas de pagamento.
+        <h3>Política de Cancelamento</h3>
+        <p>Até dia 01/09/2019 pode-se fazer uma devolução de 50% do valor  pago pela inscrição no ERRC 2019, o restante do valor é utilizado para cobrir  os custos administrativos. Após esta data não haverá devolução de qualquer valor pago. A política se aplica tanto para o cancelamento da inscrição no  evento quanto para o cancelamento de qualquer atividade adicional (minicurso,  workshop, jantar, dentre outras). Para solicitar o cancelamento, enviar um  e-mail para <a href="mailto:faturamento@sbc.org.br">faturamento@sbc.org.br</a>.
         </p>
-        <br><br>
+        </div>
+</div> <!-- FIM CANCELAMENTO -->
+        
+        
+        
+        
     </div>
 </div> <!-- FIM CONTEUDO INSCRIÇÕES -->
 
