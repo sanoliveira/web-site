b6a8dccc87b60b539adb5bbaff9133be376cfe2b
K D <kreutz@acm.org>
2019-08-15 17:05:45 -0300

    last updates done at Thu Aug 15 17:05:45 -03 2019

--- programacao.php
+++ programacao.php
@@ -71,7 +71,7 @@
     
     <h3>Introdução à Tecnologia dos Blockchains: Teoria e Prática</h3>
     <p><b>Data:</b> a ser definida </p>
-    <p><b>Autores:</b> João Otávio Chervinski, Felipe Melchior, Rafael Fernandes, Guilherme Neri, Lucas Antunes, Diego Kreutz, Rodrigo Mansilha</p>
+    <p><b>Autores:</b> João Otávio Chervinski, Felipe Melchior, Rafael Fernandes, Guilherme Neri, Lucas Antunes, Diego Kreutz, Rodrigo Mansilha (UNIPAMPA)</p>
     
      <p>
         <b>Resumo:</b> O surgimento e a difusão das criptomoedas, como a Bitcoin, causaram uma disrrupção no mercado financeiro mundial ao permitir a realização de pagamentos para qualquer lugar do planeta sem a necessidade de autoridades reguladoras. Uma das inovações trazidas por criptomoedas foi uma estrutura de dados descentralizada denominada de Blockchain. Essa estrutura permite armazenar as informações de maneira descentralizada, nos computadores dos usuários da criptomoeda, e assim elimina a necessidade de entidades centralizadoras de controle, como e ́ o caso dos tradicionais bancos. Os Blockchains também propõem a utilização de mecanismos simples para garantir determinadas propriedades de segurança ̧a, como a integridade de dados. Combinando a aplicação de criptografia assimétrica e o conceito de provas-de-trabalho, Blockchains garantem a integridade dos dados de uma rede formada por participantes anônimos. Embora a maior aplicação dos Blockchains seja em criptomoedas, a tecnologia despertou interesse na academia e na indústria, fomentando a sua aplicação em áreas como financeira, logística, cadeia de suprimentos, energia e Internet das coisas. O objetivo deste minicurso é difundir os conceitos e potenciais casos de aplicação da tecnologia de Blockchain. Com o minicurso, esperamos instruir estudantes e profissionais de computação sobre a tecnologia e estimular o desenvolvimento de pesquisas na área. Durante o minicurso, serão apresentados conceitos teóricos fundamentais e realizadas demonstrações pra ́ticas através de um sistema de Blockchain desenvolvido para fins didáticos.
@@ -82,7 +82,7 @@
         
     <h3>Introdução à Propriedades Avançadas de Segurança da Informação</h3>
     <p><b>Data:</b> a ser definida </p>
-    <p><b>Autores:</b> Diego Kreutz, Sabrina Carlé Winckler, Rodrigo de Oliveira Barbosa, João Otávio Chervinski, Tadeu Jenuário</p>
+    <p><b>Autores:</b> Diego Kreutz, Sabrina Carlé Winckler, Rodrigo de Oliveira Barbosa, João Otávio Chervinski, Tadeu Jenuário (UNIPAMPA)</p>
     
      <p>
         <b>Resumo:</b> Em protocolos de segurança tradicionais, assumimos que não há mais o que fazer se a chave secreta, compartilhada entre os usuários Alice e Bob, que estão trocando mensagens entre si, for comprometida. De fato, se considerarmos apenas as propriedades básicas de segurança (e.g. confidencialidade, integridade e autenticidade), tanto comunicações passadas quanto futuras serão comprometidas no momento em que a chave secreta for comprometida. Entretanto, existem propriedades e características avançadas de segurança, como Perfect Forward Secrecy (PFS), Post-Compromise Recovery (PCR) e Post-Compromise Secure (PCS) que permitem a construção de protocolos de segurança mais resilientes ao comprometimento de chaves secretas. Enquanto PFS protege comunicações passadas, PCR e PCS têm por objetivo recuperar o protocolo a um estado seguro e proteger comunicações futuras, respectivamente. Adicionalmente, outra propriedade desejada no projeto de protocolos atuais e futuros é a PQS (Post-Quantum Secure). Esta propriedade assegura que os protocolos não serão alvos fáceis de computadores quânticos, que são capazes de quebrar a segurança de protocolos tradicionais, como o RSA e o Diffie-Hellman, utilizando apenas ataques de força-bruta. Como estas propriedades de segurança são pouco exploradas na literatura, ou recentes como é o caso da PCR, PCS e PQS, neste minicurso o nosso objetivo é apresentar uma visão geral, de forma didática e progressiva, de propriedades básicas e avançadas de segurança no projeto de protocolos e sistemas mais robustos. Além disso, nós apresentaremos exemplos reais de protocolos e sistemas que utilizam as diferentes propriedades de segurança discutidas. Finalmente, iremos também demonstrar, de forma simples e didática, a importância da utilização de ferramentas formais para verificação automática de protocolos de segurança, como os apresentados durante o minicurso.
@@ -93,7 +93,7 @@
     
     <h3>Introdução à linguagem de programação P4, o futuro das redes</h3>
     <p><b>Data:</b> a ser definida </p>
-    <p><b>Autores:</b> Alisson B. Zanetti, Pedro E. Camera, Marco A. Trentin, Ricardo O. Schmidt</p>
+    <p><b>Autores:</b> Alisson B. Zanetti, Pedro E. Camera, Marco A. Trentin, Ricardo O. Schmidt (UPF)</p>
     
     <p>
     <b>Resumo:</b>As redes de computadores tradicionais enfrentam grandes desafios de manutenibilidade frente às demandas atuais por conexões rápidas e dinâmicas. O crescente uso de recursos de rede nos seus mais variados níveis, aliado ao privilégio de configurações por parte dos fabricantes, tornaram o emprego das arquiteturas conhecidas uma pra ́tica engessada, complexa e pouco fértil para novas aplicações. Utilizando-se de uma compreensão que separa os módulos de controle das demais funções de rede, definindo-os por meio de software, o paradigma SDN quebra a integração vertical entre a gerência de rede e a camada por onde os dados trafegam, proporcionando o controle centralizado dos equipamentos, abstraindo muito da complexidade empregada nos ambientes de rede, quer por políticas dos fabricantes, quer pela natureza da arquitetura tradicional. Este minicurso tem como objetivo demonstrar o funcionamento destas novas tecnologias, com ênfase na programação do plano de dados por meio da linguagem P4, utilizando-se de ambientes simulados e manipulados pelo próprio cursista, compreendendo assim o imenso universo de possibilidades a serem exploradas nesta nova dinâmica por trás das redes futuras. 
@@ -104,7 +104,7 @@
         
     <h3>Análise de tráfego de máquinas virtuais na rede de controle de nuvens computacionais baseadas em OpenStack</h3>
     <p><b>Data:</b> a ser definida </p>
-    <p><b>Autores:</b> Adnei Willian Donatti, Charles Christian Miers, Guilherme Piêgas Koslovski, Maurício Aronne Pillon</p>
+    <p><b>Autores:</b> Adnei Willian Donatti, Charles Christian Miers, Guilherme Piêgas Koslovski, Maurício Aronne Pillon (UDESC)</p>
     
     <p>
     <b>Resumo:</b>A computação em nuvem é um paradigma para disponibilização de infraestrutura computacional. Este paradigma muda a localização da infraestrutura para a rede, no sentido de possibilitar a distribuição ou realocação de carga entre os diversos recursos computacionais conectados a rede. Portanto, emprega-se virtualização para reduzir os custos relacionados com o gerencia- mento e manutenção de recursos de hardware e software [Vaquero et al. 2008, Mell and Grance 2011]. Segundo [Jadeja and Modi 2012], o usuário pode ter acesso a uma infraestrutura virtualizada externa mediante pagamento pelo uso, ou com a implantação de uma nuvem privada, cuja utilização alinha-se às necessidades de uma organização. Segundo [Wang and Ng 2010], questões relacionadas ao desempenho de uma nuvem dependem da identificação de comportamentos e operações que ocorrem durante o seu uso. Além disso, percebe-se, que há uma necessidade de identificar operações, muitas das quais são operações que ocorrem no nível da camada de rede e demandam análise e compreensão do que está trafegando e sua finalidade. Neste sentido, a caracterização de tráfego auxilia este entendimento, por meio do emprego de técnicas e métodos que possibilitam a coleta e identificação de forma sistematizada. Apesar de não ter sido identificada uma definição formal ou me ́todo para caracterização de tráfego, este termo refere-se à minuciosa análise do tráfego de uma rede, buscando entender seu comportamento e também as consequências que derivam deste comportamento. 
@@ -115,7 +115,7 @@
         
     <h3>Web Application Firewalls (WAFs): Teoria e Prática</h3>
     <p><b>Data:</b> a ser definida </p>
-    <p><b>Autores:</b> Felipe Melchior, Diego Kreutz, Mauricio Fiorenza, Fernando Flora, Douglas Macedo</p>
+    <p><b>Autores:</b> Felipe Melchior, Diego Kreutz, Mauricio Fiorenza, Fernando Flora (UNIPAMPA), Douglas Macedo (UFSC)</p>
     
     <p>
     <b>Resumo:</b> Relatórios especializados indicam que aproximadamente 90% das aplicações disponibilizadas na Web apresentam algum tipo de vulnerabilidade. A boa notícia é que estudos recentes apontam que frameworks de desenvolvimento de software e ferramentas de segurança como Web Application Firewalls conseguem mitigar a exploração de mais de 70% das vulnerabilidades de uma aplicação Web. Neste contexto, o objetivo deste minicurso é demonstrar, na teoria e na prática, como a utilização, a escolha e a otimização de WAFs pode contribuir de forma significativa na proteção de aplicações Web contra os principais tipos de ataques que buscam explorar as vulnerabilidades mais recorrentes em aplicações Web. Durante o minicurso, iremos apresentar conceitos teóricos fundamentais para a adequada compreensão, configuração e utilização de Web Application Firewalls. Além disso, iremos utilizar máquinas virtuais pré-configuradas para demonstrar na prática o funcionamento e impacto de WAFs na proteção de aplicações Web. Como resultado, esperamos contribuir de forma significativa na conscientização de estudantes e profissionais de computação sobre a importância do uso de WAFs na proteção de aplicações Web.
@@ -197,7 +197,7 @@
     
         <h3>Virtualização através de containers para plataformas de computação em nuvem</h3>
         <p><b>Data:</b> a ser definida </p>
-        <p><b>Autores:</b> Raul Leiria, Adriano Vogel, Dalvan Griebler, Claudio Schepke</p>
+        <p><b>Autores:</b> Raul Leiria, Adriano Vogel, Dalvan Griebler (PUCRS), Claudio Schepke (UNIPAMPA)</p>
         <p>
         <b>Resumo:</b> A computação em nuvem disponibiliza, sob demanda, recursos e sistemas para os usuários através de redes de computadores. Seus múltiplos modelos e abstrações permitem que o usuário escolha os recursos que mais se adequam às necessidades de suas aplicações, sem por isso perder à escalabilidade. A utilização de containers na computação em nuvem é tendência na indústria e na academia. Seu uso permite à implantação de sistemas computacionais com menores custos na utilização de recursos, se comparado com a utilização de máquinas virtuais. Em vista disso, o objetivo desta oficina é introduzir à utilização de containers Docker para computação em nuvem. A oficina tem enfoque pra ́tico ao instalar, criar e orquestrar containers em uma nuvem OpenStack. Espera-se que ao final da oficina o participante consiga configurar e operar uma nuvem privada que utilize virtualização através de containers. 
         </p> 
@@ -205,7 +205,7 @@
     
         <h3>Configuração de Switches Gerenciáveis</h3>
         <p><b>Data:</b> a ser definida </p>
-        <p><b>Autores:</b> Ronaldo Canofre M. dos Santos, Maurício M. Fiorenza</p>
+        <p><b>Autores:</b> Ronaldo Canofre M. dos Santos, Maurício M. Fiorenza (UNIPAMPA)</p>
         <p>
         <b>Resumo:</b> A aplicação do conhecimento acadêmico nas atividades práticas proporciona uma ampliação no aprendizado e conhecimento, dessa forma, este documento aborda a proposição de uma oficina a ser realizada na XVII Escola Regional de Redes de Computadores. A proposta tem como tema principal a configuração e manutenção de switches gerenciáveis, e busca proporcionar aos participantes um conhecimento teórico inicial, assim como uma experiência prática via simulador de rede.
         </p> 
@@ -213,7 +213,7 @@
     
         <h3>Hack The Box: Introdução ao Pentest</h3>
         <p><b>Data:</b> a ser definida </p>
-        <p><b>Autores:</b> Felipe Melchior, Rafael Fernandes, Diego Kreutz</p>
+        <p><b>Autores:</b> Felipe Melchior, Rafael Fernandes, Diego Kreutz (UNIPAMPA)</p>
         <p>
         <b>Resumo:</b> No contexto da sociedade moderna, que é cada vez mais dependente da tecnologia, as discussões sobre segurança ̧a de sistemas e dados vêm ganhando destaque e importância. Um dos principais motivos é o número crescente de incidentes de segurança, em especial com relação aos sistemas e dispositivos conectados à Internet. Esta oficina tem como objetivo demonstrar na prática como funcionam os testes de intrusão de sistemas online através da plataforma Hack The Box (HTB - https: //www.hackthebox.eu). Os laboratórios de pentesting disponibilizados pela HTB representam importantes recursos de formação de profissionais capazes de entender e lidar com os mais diversos tipos de desafios do ciber espaço atual. Como resultado, esperamos contribuir com a formação e conscientização de estudantes e profissionais de computação o no que diz respeito à segurança de sistemas e dados. Além disso, destacar o motivo pelo qual é necessário, cada vez mais, um profissional da área de segurança pensar como um atacante para conseguir identificar ataques e construir defesas cada vez mais robustas. 
         </p> 
