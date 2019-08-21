f44a950507d60913a679619d69fff8890d3cbbd1
K D <kreutz@acm.org>
2019-08-20 23:45:52 -0300

    last updates done at Tue Aug 20 23:45:52 -03 2019

--- trabalhos_aceitos_errc2019.php
+++ trabalhos_aceitos_errc2019.php
@@ -1,70 +1,87 @@
 <h4>LISTA PROVISÓRIA DOS PAPERS ACEITOS</h4>
 <p>
 <b>Segurança na camada de enlace</b>
+<br>
 <i>Felipe Franz (Universidade Tecnológica Federal do Paraná)</i>
 <br>
 <br>
 <b>Plataforma e-consumo: Monitoramento Inteligente do Consumo de Energia Elétrica em Residências</b>
+<br>
 <i>Larissa Del Rio, Andre Eichner, Tiago Antônio Rizzetti (Universidade Federal de Santa Maria)</i>
 <br>
 <br>
 <b>Fog Computing: análise de desempenho computacional</b>
+<br>
 <i>Leonardo Foletto, Ivania Fischer, Roseclea Medina, Eder Pereira (Universidade Federal de Santa Maria)</i>
 <br>
 <br>
 <b>Zabbix Monitoring Tool Optimization at Hospital Universitário de Santa Maria</b>
+<br>
 <i>Vinícius Röpke, Matheus Guarienti, Tiago Antonio Rizzetti (Universidade Federal de Santa Maria)</i>
 <br>
 <br>
 <b>Análise do Desempenho de Heurísticas na Coleta de Informações de Telemetria In-Band</b>
+<br>
 <i>Ariel Góes de Castro, Francisco Vogt, Victor Hugo Lopes, Sérgio Rossi da Silva, Arthur Lorenzon (Universidade Federal do Pampa), Fabio Rossi (Instituto Federal Farroupilha), Marcelo Caggiani Luizelli (Universidade Federal do Pampa)</i>
 <br>
 <br>
 <b>Proposta de Monitoramento e Gerenciamento Inteligente de Temperatura em Datacenters</b>
+<br>
 <i>Djalma Teixeira (Sociedade Educacional Três de Maio - SETREM), Adriano Vogel (PUCRS), Dalvan Griebler (PUCRS/SETREM)</i>
 <br>
 <br>
 <b>Gestão de Privacidade de Dados do Paciente em Registros Médico-Hospitalares Eletrônicos</b>
+<br>
 <i>Fabio Demo, Walter Priesnitz Filho (Universidade Federal de Santa Maria - UFSM)</i>
 <br>
 <br>
 <b>Análise do Desempenho de VPNs nas Camadas de Rede e Enlace</b>
+<br>
 <i>Daniel Stasiak, Walter Priesnitz Filho (Universidade Federal de Santa Maria - UFSM)</i>
 <br>
 <br>
 <b>Projeto redes: implementação de uma MANET para a conexão de dispositivos IoT utilizando protocolo OLSR</b>
+<br>
 <i>Joao Mário Brezolin, Cristiano Nunes (Instituto Federal Sul-Rio-Grandense - IFSul)</i>
 <br>
 <br>
 <b>LAZE - Um poderoso analisador de logs do Squid</b>
+<br>
 <i>Natália Knob (IFRS), Luis Knob (Pontifícia Universidade Católica do Rio Grande do Sul - PUCRS), Eduardo Germano da Silva (Universidade Federal do Rio Grande do Sul - UFRGS), Cristian Cleder Machado (Universidade Regional Integrada (URI) - Câmpus de Frederico Westphalen), Diego Lusa(IFRS - Campus Sertão)</i>
 <br>
 <br>
 <b>Uma Comparação dos Dados da Ferramenta Zabbix com a Percepção dos Usuários da Rede no Instituto Federal Catarinense ? Campus Avançado Sombrio</b>
+<br>
 <i>Liliana Colares, Matheus Braga, Victor Martins de Sousa, Diuliana da Rosa (Instituto Federal Catarinense - Campus Sombrio - IFC Sombrio)</i>
 <br>
 <br>
 <b>Avaliação do uso de Smart Contracts para Sistema de Saúde Colaborativa</b>
+<br>
 <i>Vinícius Branco, Bruno Lippert, Henry Nunes (PUCRS), Roben Lunardi (Instituto Federal do Rio Grande do Sul - IFRS), Avelino Zorzo(PUCRS)</i>
 <br>
 <br>
 <b>Firewalls em Redes Definidas por Software: Estado da Arte</b>
+<br>
 <i>Maurício Fiorenza, Diego Kreutz (Universidade Federal do Pampa - UNIPAMPA)</i>
 <br>
 <br>
 <b>Análise de segurança no uso de contêineres Docker em máquinas virtuais</b>
+<br>
 <i>Kerolayne Oliveira, Charles Miers (Universidade do Estado de Santa Catarina - UDESC)</i>
 <br>
 <br>
 <b>Um Algoritmo de Balanceamento de Carga para a Computação em Névoa</b>
+<br>
 <i>Eder Pereira, Moisés Oliveira, Ivania Fischer (UFSM), Edson Luiz Padoin (UNIJUI/UFRGS), Roseclea Medina (Universidade Federal de Santa Maria - UFSM)</i>
 <br>
 <br>
 <b>O Impacto Sustentável das Redes Móveis Densas Heterogêneas no Futuro 5G - Levantamento Bibliográfico</b>
+<br>
 <i>Luis Eduardo Simioni Centeleghe, Luis Felipe dos Santos Vasconcelos (Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul - Campus Canoas)</i>
 <br>
 <br>
 <b>Definindo um Protocolo de Autenticação Utilizando Bluetooth Low Energy para Dispositivos no Conceito de IoT</b>
+<br>
 <i>Andre Eichner, Nathan da Silva, Tiago Antônio Rizzetti (Universidade Federal de Santa Maria - UFSM)</i>
 <br>
 <br>
@@ -72,26 +89,32 @@
 <h4>LISTA PROVISÓRIA DOS RESUMOS ESTENDIDOS ACEITOS</h4>
 <p>
 <b>Registro de eventos de alocação de contêineres em Blockchain</b>
+<br>
 <i>Marco Marques, Charles Miers (Universidade do Estado de Santa Catarina - UDESC)</i>
 <br>
 <br>
 <b>Uma Investigação sob a Percepção dos Usuários no Desempenho da Rede no Instituto Federal Catarinense - Campus Avançado Sombrio</b>
+<br>
 <i>Diuliana de Matos da Rosa, Liliana Campos Colares, Matheus Lorenzato Braga, Victor Martins de Sousa (Instituto Federal Catarinense - Campus Avançado Sombrio)</i>
 <br>
 <br>
 <b>Avaliação de Desempenho de Heavy Hitters utilizando P4 e XDP</b>
+<br>
 <i>Douglas Machado, Ariel Góes de Castro, Francisco Vogt, Marcelo Caggiani Luizelli (Universidade Federal do Pampa - UNIPAMPA)</i>
 <br>
 <br>
 <b>Composição Eficiente de Encadeamento de Funções de Serviços</b>
+<br>
 <i>Igor Capeletti (Universidade Federal do Pampa - UNIPAMPA), Fabio Rossi (Instituto Federal Farroupilha - Campus Alegrete), Marcelo Caggiani Luizelli (Universidade Federal do Pampa - UNIPAMPA)</i>
 <br>
 <br>
 <b>Rumo à visibilidade total da rede: uma estratégia adaptativa para coordenadar a coleta de dados de telemetria in-band</b>
+<br>
 <i>Rumenigue Hohemberger, Fabio Rossi (Instituto Federal Farroupilha - Campus Alegrete), Marcelo Caggiani Luizelli (Universidade Federal do Pampa - UNIPAMPA)</i>
 <br>
 <br>
 <b>Rumo á Delegação Escalável e Segura de Gerenciamento de LANs para ISPs</b>
+<br>
 <i>Daniel Temp, Rodrigo Mansilha, (Universidade Federal do Pampa - UNIPAMPA), Deivid Rodrigues (Interneith), Diego Kreutz (Universidade Federal do Pampa - UNIPAMPA)</i>
 <br>
 <br>
